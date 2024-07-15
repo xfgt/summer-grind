f18322bb7ec5fa2c4c2e621fc6ca48fb325cdbb6
xfgt <waffle_man562@yahoo.com>
2024-07-15 06:48:40 +0300

    error management

--- Exercises/Objects and Classes - Advanced_Exercise/07_BytesParsing/BytesParsing.cpp
+++ Exercises/Objects and Classes - Advanced_Exercise/07_BytesParsing/BytesParsing.cpp
@@ -3,6 +3,15 @@
 //
 #include "BytesParsing.h"
 
+#include <pstl/execution_defs.h>
+
+
+void printVector(const std::vector<long long>& vec){
+    for(const auto& it : vec)
+        std::cout << it << " ";
+}
+
+
 
 extern ErrorCode parseData (const std::string& commands,
                             const char* rawDataBytes,
@@ -11,15 +20,12 @@ extern ErrorCode parseData (const std::string& commands,
 
 {
 
+
+    if(rawDataBytesCount == 0 || commands.empty())
+        return PARSE_EMPTY;
+
 // rawData -> char - 30;
 
-/* cheta bukvata ot stringa
- * s neq postavqm granica:
- * s -> short = 2
- * i -> int = 4
- * l -> longlong 8
- *
- */
     int j{};
     int k{};
     for(int i = 0; i < commands.size(); i++){
@@ -34,36 +40,52 @@ extern ErrorCode parseData (const std::string& commands,
             case 'l':   // long -> 64 bits (8*8)
                 iterations = 8;
                 break;
+
             default:
-                break;
+                break;  // TODO: Return error if bad sequence occurs;
+                        // TODO: Keep in mind that the command buffer may contain commands, which you have no data for in your data buffer.
+                        // TODO: When you reach such case -> simply ignore the rest of the commands from the command buffer.
         }
 
-        if(iterations == 0) continue;
+        if(iterations == 0) return PARSE_FAILURE;
 
 
-        //from front to back ss, 2002 -> 200 (16) -> 512, 2(16) -> 2 [push front]
+
+
+        //convertion and "upload"
         long long number{};
         for(j = k; j < rawDataBytesCount; j++){
             for(int f = 0; f < iterations; f++, k++){
+                if(k > rawDataBytesCount) return PARSE_FAILURE;
                 number += rawDataBytes[k];
             }
             break;
         }
         outParsedNumbers.push_back(number);
-        outParsedNumbers.insert(outParsedNumbers.begin(), number); // "push front"
 
-
-
-
+        //from front to back ss, 2002 -> 200 (16) -> 512, 2(16) -> 2 [push front]
+        //outParsedNumbers.insert(outParsedNumbers.begin(), number); // "push front"
     }
 
-
-
+    return PARSE_SUCCESS;
 }
 
 extern void printResult (const ErrorCode errorCode, const std::vector<long long>& parsedNumbers) {
-
-
+    switch(errorCode){
+    case PARSE_EMPTY: // don't print vector at all
+            std::cout << "No input provided" << std::endl;
+            break;
+        case PARSE_SUCCESS:
+            printVector(parsedNumbers);
+        std::cout << std::endl;
+            break;
+    case PARSE_FAILURE:
+        printVector(parsedNumbers);
+        std::cout << "Warning, buffer underflow detected" << std::endl;
+            break;
+        default:
+            break;
+    }
 
 
 }
