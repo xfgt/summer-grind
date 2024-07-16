3c4a146592f81766b52dcc729eb56ea6f5f798ba
xfgt <waffle_man562@yahoo.com>
2024-07-16 16:27:12 +0300

    solution 70/100

--- Exercises/Objects and Classes - Advanced_Exercise/07_BytesParsing/BytesParsing.cpp
+++ Exercises/Objects and Classes - Advanced_Exercise/07_BytesParsing/BytesParsing.cpp
@@ -8,52 +8,45 @@
 
 
 void printVector(const std::vector<long long>& vec){
-    for(const auto& it : vec)
+    for (const auto& it : vec)
         std::cout << it << " ";
 }
 
 
 int hexCalc(const std::string& str){
-
     int decimal{};
     int result{};
     char digit{};
-    for(int i = 0, j = 0; i < str.size(); i++, j += 2){
-
+    for (int i = 0, j = 0; i < str.size(); i++, j += 2){
         digit = str[i];
-        if(digit == '0') continue;
+        if (digit == '0') continue;
         decimal = std::stoi(&digit);
         result += decimal * pow(16, j);
-
     }
     return result;
 }
 
 
-
-extern ErrorCode parseData (const std::string& commands,
-                            const char* rawDataBytes,
-                            const size_t rawDataBytesCount,
-                            std::vector<long long>& outParsedNumbers)
-
-{
-
-    if(rawDataBytesCount == 0 || commands.empty())
+extern ErrorCode parseData(const std::string& commands,
+                           const char* rawDataBytes,
+                           const size_t rawDataBytesCount,
+                           std::vector<long long>& outParsedNumbers){
+    if (rawDataBytesCount == 0 || commands.empty())
         return PARSE_EMPTY;
 
 
     int j{};
     int k{};
-    for(int i = 0; i < commands.size(); i++){
+    for (int i = 0; i < commands.size(); i++){
         int iterations{};
         switch (commands[i]){
-        case 's':   // short -> 16 bits (2*8)   [base 16]
+        case 's': // short -> 16 bits (2*8)   [base 16]
             iterations = 2;
             break;
-        case 'i':   // int -> 32 bits (4*8)
+        case 'i': // int -> 32 bits (4*8)
             iterations = 4;
             break;
-        case 'l':   // long -> 64 bits (8*8)
+        case 'l': // long -> 64 bits (8*8)
             iterations = 8;
             break;
 
@@ -61,16 +54,16 @@ extern ErrorCode parseData (const std::string& commands,
             break;
         }
 
-        if(iterations == 0) return PARSE_FAILURE;
-
+        if (iterations == 0) return PARSE_FAILURE;
 
         //convertion and "upload"
         std::string number("");
-        for(j = k; j < rawDataBytesCount; j++){
-            for(int f = 0; f < iterations; f++, k++){
-                if(k >= rawDataBytesCount) return PARSE_FAILURE;
-                if(rawDataBytes[k]+'0' >= '0' && rawDataBytes[k]+'0' <= '9')
-                    number += rawDataBytes[k]+'0';
+        for (j = k; j < rawDataBytesCount; j++){
+            for (int f = 0; f < iterations; f++, k++){
+                if (k >= rawDataBytesCount) return PARSE_FAILURE;
+                if (rawDataBytes[k] + '0' >= '0' && rawDataBytes[k] + '0' <= '9')
+                    number += rawDataBytes[k] + '0';
+                else return PARSE_SUCCESS;
             }
 
             outParsedNumbers.push_back(hexCalc(number));
@@ -78,29 +71,24 @@ extern ErrorCode parseData (const std::string& commands,
 
             break;
         }
-
-
-
     }
 
     return PARSE_SUCCESS;
 }
 
-extern void printResult (const ErrorCode errorCode, const std::vector<long long>& parsedNumbers) {
-    switch(errorCode){
+extern void printResult(const ErrorCode errorCode, const std::vector<long long>& parsedNumbers){
+    switch (errorCode){
     case PARSE_EMPTY:
-            std::cout << "No input provided";
-            break;
-        case PARSE_SUCCESS:
-            printVector(parsedNumbers);
-            break;
+        std::cout << "No input provided";
+        break;
+    case PARSE_SUCCESS:
+        printVector(parsedNumbers);
+        break;
     case PARSE_FAILURE:
         printVector(parsedNumbers);
         std::cout << "Warning, buffer underflow detected";
-            break;
-        default:
-            break;
+        break;
+    default:
+        break;
     }
-
-
 }
