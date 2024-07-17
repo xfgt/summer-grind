71ca368989be257e47fb4b47b494fb50fa9bc057
xfgt <waffle_man562@yahoo.com>
2024-07-17 11:51:34 +0300

    01_Compile: Solution with img

---
+++ Exercises/Code Organization and Templates - Lab/01_Compile/Defines.h
@@ -0,0 +1,15 @@
+//
+// Created by twister on 7/17/24.
+//
+
+#ifndef DEFINES_H
+#define DEFINES_H
+
+    #ifndef DONT_COMPILE_THIS
+        #define DONT_COMPILE_THIS
+        #define STANDARD_TEMPLATE_LIBRARY namespace std
+        #include <iostream>
+    #endif
+
+
+#endif //DEFINES_H
