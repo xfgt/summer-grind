ba5b9b46203a095dbf5e4e6b6a62b327d37452fd
xfgt <waffle_man562@yahoo.com>
2024-07-20 15:05:41 +0300

    checkpoint extraction

--- Exercises/Code Organization and Templates - Lab/04_SplitAndJoin/Join.h
+++ Exercises/Code Organization and Templates - Lab/04_SplitAndJoin/Join.h
@@ -6,7 +6,33 @@
 #define JOIN_H
 // returns a std::string
 #include <iostream>
+#include <sstream>
 #include <vector>
+template<typename T>
+std::string join(const std::vector<T>& v, const std::string& theNewSep){
+    std::string result{};
+    std::string sample{};
+    std::ostringstream os(sample);
 
+    for(auto it = v.begin(); it != v.end(); ++it){
+        T type = *it;
+        os << type;
+
+        std::istringstream s(os.str()); // result += os.str();
+        s  >> result;
+        os << theNewSep;
+
+    }
+
+
+
+
+
+
+
+
+
+    return result;
+}
 
 #endif //JOIN_H
