c844f471c403ed0929e5efe4156402dfc5ffbde9
xfgt <waffle_man562@yahoo.com>
2024-07-27 12:53:27 +0300

    zero tests pass

--- Exercises/Code Organization and Templates - Lab/05_Sorting/Comparators.h
+++ Exercises/Code Organization and Templates - Lab/05_Sorting/Comparators.h
@@ -14,21 +14,22 @@
 
 template<typename T> // functor (old lambda)
 struct LessThan {
-    bool operator()(const T& a, const T& b){
+    bool operator()(const T& a, const T& b) const {
 
-        std::stringstream ss;
-        std::string x, y{};
-
-        ss << a;
-        x = ss.str();
-
-        ss.str("");
-        ss.clear();
-
-        ss << b;
-        y = ss.str();
-
-        return (std::strcmp(x.c_str(), y.c_str()) < 0);
+        // std::stringstream ss;
+        // std::string x, y{};
+        //
+        // ss << a;
+        // x = ss.str();
+        //
+        // ss.str("");
+        // ss.clear();
+        //
+        // ss << b;
+        // y = ss.str();
+        //
+        // return (std::strcmp(x.c_str(), y.c_str()) < 0);
+        return a < b;
 
     }
 };
@@ -36,7 +37,7 @@ struct LessThan {
 
 template <typename T, class LT>
 struct Reverse{
-    bool operator()(const T& left, const T& right){
+    bool operator()(const T& left, const T& right) const {
         LT lt;
         return !lt(left, right);
     }
