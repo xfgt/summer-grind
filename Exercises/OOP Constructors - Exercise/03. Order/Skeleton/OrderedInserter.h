4fb1dddb61ef79131087f82ff9110a8dc482ab3c
xfgt <waffle_man562@yahoo.com>
2024-07-29 16:59:32 +0300

    almost sort

--- Exercises/OOP Constructors - Exercise/03. Order/Skeleton/OrderedInserter.h
+++ Exercises/OOP Constructors - Exercise/03. Order/Skeleton/OrderedInserter.h
@@ -6,6 +6,8 @@
 #define ORDEREDINSERTER_H
 
 
+#include <algorithm>
+
 #include "Company.h"
 #include <vector>
 
@@ -13,28 +15,42 @@
 struct OrderedInserter{
 
     const Company* current;
-    std::vector<const Company*>::iterator it;
     std::vector<const Company*>& m_inputVector;
+    std::vector<const Company*>::iterator it;
+
 
     explicit OrderedInserter(std::vector<const Company*>& xv) :
     m_inputVector(xv),
-    current(nullptr) {}
+    current(nullptr),
+    it(nullptr){}
 
     void insert(const Company* x){
+
         if(current == nullptr){
             m_inputVector.push_back(x);
             it = m_inputVector.begin();
+            current = x;
+            return;
         }
-        else m_inputVector.insert(it++, x);
+
+        if(x->getId() < current->getId()){
+            m_inputVector.insert(m_inputVector.begin(), x);
+        } else{
+            if(it-1 < m_inputVector.begin()){
+                m_inputVector.push_back(x);
+            } else { // mid insert
+
+                m_inputVector.insert(current position iterator + 1, x);
 
 
+
+            }
+
+        }
+        it = m_inputVector.end()-1; /// !
         current = x;
-
     }
 
-    bool operator<(const Company* sample) const {
-        return sample->getId() < current->getId();
-    }
 
 
 };
