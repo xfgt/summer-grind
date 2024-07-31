775a769a496e9a6de695b087a511e728b1b18ace
xfgt <waffle_man562@yahoo.com>
2024-07-31 14:16:33 +0300

    solution [no sort]

--- Exercises/OOP Constructors - Exercise/04. Profits/Skeleton/ProfitReport.h
+++ Exercises/OOP Constructors - Exercise/04. Profits/Skeleton/ProfitReport.h
@@ -12,64 +12,43 @@
 
 
 struct getProfitReport{
+    friend std::ostream& operator << (std::ostream&, const getProfitReport&);
+
+
+
 
     std::map<std::string, int> m_results;
-
-
 
     const Company* m_start;
     const Company* m_finish;
     typedef std::map<int, ProfitCalculator> theMap;
     theMap& m_data;
 
-
     explicit getProfitReport(const Company* start, const Company* finish, theMap& data) :
         m_start(start),
         m_finish(finish),
         m_data(data) {
 
-
-        // find by id
-        // it.second.calc() == * profit calcs by comp tax percentage
-
-        // get companies vector iterate by id and calc values with certain
-        // company tax percentage
-
-
-        // iterate by memory from start to finish (m_)
-
         const Company* end = m_finish+1;
 
-
         for(const auto& it : m_data){
-            for(auto& x = m_start; x != end; ++x){
-            //      std::cout << *x << std::endl;
+            auto x = m_start; // no ref!!
+            while (x != end){
                 if(it.first == x->getId()){
                     int result = it.second.calculateProfit(*x);
                     m_results[x->getName()] = result;
                     break;
                 }
+                ++x;
             }
-
         }
-
-
-
-
-
-    }
-
-
-
-    friend std::ostream& operator << (std::ostream&, const getProfitReport&);
-
+    }// !Constructor
 
 };
 
-std::ostream& operator << (std::ostream& OUT, const getProfitReport& obj ){
-    //obj.m_results
+inline std::ostream& operator << (std::ostream& OUT, const getProfitReport& obj ) {
     for(const auto& it : obj.m_results){
-        OUT << &it << std::endl;
+        OUT << it.first << " = " << it.second << std::endl;
     }
     return OUT;
 }
