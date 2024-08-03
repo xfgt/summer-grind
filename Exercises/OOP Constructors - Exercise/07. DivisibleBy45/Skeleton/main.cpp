a0855334907714e907d618e945b7092c534a01bc
xfgt <waffle_man562@yahoo.com>
2024-08-03 15:50:05 +0300

    checkpoint

--- Exercises/OOP Constructors - Exercise/07. DivisibleBy45/Skeleton/main.cpp
+++ Exercises/OOP Constructors - Exercise/07. DivisibleBy45/Skeleton/main.cpp
@@ -18,10 +18,19 @@ int main(){
     BigInt bOne(a);
     BigInt bTwo(b);
 
+
     int start, end{};
 
     try{
-        std::stoi(bOne.getDigits());
+
+        std::stoi(bOne.getDigits()); // check
+        std::stoi(bTwo.getDigits()); // check
+
+        // {
+        //     bOne = bOne.getDigits().replace(0, 1, "0");
+        //     bTwo = bTwo.getDigits().replace(0, 1, "0");
+        // }
+
         start = std::stoi(bOne.getDigits());
         end = std::stoi(bTwo.getDigits());
 
@@ -36,8 +45,7 @@ int main(){
         start = std::stoi(a.substr(a.size()-2));
         end = std::stoi(b.substr(b.size()-3));
 
-
-        for(int i = start; i < end;  i++){
+        for(int i = start; i < end; i++){
             if(i % 45 == 0){
                 std::cout << bOne.getDigits().replace(bOne.getDigits().size()-2, 3, std::to_string(i)) << std::endl;
             }
