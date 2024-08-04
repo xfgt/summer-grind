b9a4fb144abe67b2b3d5c8b1e0a78f27e25f7482
xfgt <waffle_man562@yahoo.com>
2024-08-04 14:34:03 +0300

    zero-tests pass and runtime errors

--- Exercises/OOP Constructors - Exercise/07. DivisibleBy45/Skeleton/main.cpp
+++ Exercises/OOP Constructors - Exercise/07. DivisibleBy45/Skeleton/main.cpp
@@ -18,43 +18,50 @@ int main(){
     BigInt bOne(a);
     BigInt bTwo(b);
 
-
     int start, end{};
 
-    try{
+    try {
 
         std::stoi(bOne.getDigits()); // check
         std::stoi(bTwo.getDigits()); // check
 
-        // {
-        //     bOne = bOne.getDigits().replace(0, 1, "0");
-        //     bTwo = bTwo.getDigits().replace(0, 1, "0");
-        // }
-
         start = std::stoi(bOne.getDigits());
         end = std::stoi(bTwo.getDigits());
 
+        if(start > 101 || end > 101)
+            throw std::out_of_range("");
 
-        for(int i = start; i < end;  i++){
+        if(end < start){
+            throw std::runtime_error("");
+        }
+
+        for(int i = start; i < end; i++){
             if(i % 45 == 0){
                 std::cout << i << std::endl;
             }
         }
-    }
-    catch (...){
+
+    } catch (std::out_of_range&) {
         start = std::stoi(a.substr(a.size()-2));
-        end = std::stoi(b.substr(b.size()-3));
+        end = std::stoi(b.substr(b.size()-2));
+
+        if(end < start){
+            if(end == 0)
+                end = 100;
+            else
+                throw std::runtime_error("");
+        }
 
         for(int i = start; i < end; i++){
             if(i % 45 == 0){
                 std::cout << bOne.getDigits().replace(bOne.getDigits().size()-2, 3, std::to_string(i)) << std::endl;
             }
         }
+
     }
 
 
 
-
-
     return 0;
+
 }
