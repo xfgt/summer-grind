57a4a02c3c25c80ac55ede5bed0b919f626cf03e
xfgt <waffle_man562@yahoo.com>
2024-08-06 18:45:30 +0300

    reset function introduced

--- Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
+++ Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
@@ -18,7 +18,7 @@ struct Sequence{
     std::vector<int>::iterator m_end;
 
     Sequence(){
-        m_xv.reserve(2);
+        m_xv.reserve(3); // !
         m_begin = m_xv.begin();
         m_end = m_xv.end();
     }
@@ -27,21 +27,23 @@ struct Sequence{
     auto begin() { return m_begin; }
     auto end() { return m_end; }
 
-
     void reset(){
-        if(m_begin == m_end){
+        if(m_xv.empty()){
+            m_begin = m_xv.begin();
             m_end = m_xv.end();
-        } else{
-            m_end = m_xv.end()-1;
+
+        } else {
+            m_begin = m_xv.begin();
+            --m_end;
         }
-    }
+
+    } // problem with begin
 
     void generateNext(int& n){
 
         for(int i = 0; i < n; i++){
             m_xv.push_back(m_gen());
         }
-
         reset();
     }
 
