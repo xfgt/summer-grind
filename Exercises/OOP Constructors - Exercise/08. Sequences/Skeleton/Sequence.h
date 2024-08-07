faa6ca1fddeb88eb5e8f08035f239b917cfc7fe1
xfgt <waffle_man562@yahoo.com>
2024-08-07 13:03:53 +0300

    stream last element problem

--- Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
+++ Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
@@ -15,28 +15,34 @@ struct Sequence{
     Generator m_gen;
     std::vector<int> m_xv;
     std::vector<int>::iterator m_begin;
-    std::ostringstream m_oss;
+
 
     auto begin() { return m_begin; }
-    auto end() { return m_xv.end(); }
+    auto end() { return m_xv.end()-1; } // x-1!
 
     void generateNext(int& n){
-        if(n == 0)
+
+        std::stringstream m_ss;
+        if(n == 0){
+            m_begin = m_xv.end()-1;
             return;
+        }
+
 
         if(!(m_xv.empty()))
             m_xv.clear();
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i <= n; i++){ // = (!!!)
             int x = m_gen();
+            m_ss << x << " ";
             m_xv.push_back(x);
-            m_oss << x << " ";
         }
 
 
         m_begin = m_xv.begin(); // !!!
 
-        std::cout << m_oss.str();
+        std::cout << m_ss.str();
+
     }
 
 };
