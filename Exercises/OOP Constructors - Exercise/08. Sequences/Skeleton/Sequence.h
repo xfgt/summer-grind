413ded16f1a435dec595c004de1a95f2e2ca06f7
xfgt <waffle_man562@yahoo.com>
2024-08-07 13:45:54 +0300

    checkpoint

--- Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
+++ Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
@@ -15,33 +15,29 @@ struct Sequence{
     Generator m_gen;
     std::vector<int> m_xv;
     std::vector<int>::iterator m_begin;
-
+    std::stringstream m_ss;
 
     auto begin() { return m_begin; }
-    auto end() { return m_xv.end()-1; } // x-1!
+    auto end() { return --m_xv.end(); } // x-1!
 
     void generateNext(int& n){
 
-        std::stringstream m_ss;
-        if(n == 0){
-            m_begin = m_xv.end()-1;
-            return;
+
+        if(!(m_xv.empty())){
+            std::cout << m_ss.str();
+            m_xv.clear();
         }
 
-
-        if(!(m_xv.empty()))
-            m_xv.clear();
 
         for(int i = 0; i <= n; i++){ // = (!!!)
             int x = m_gen();
-            m_ss << x << " ";
+            m_ss << "(" << x << ") ";
             m_xv.push_back(x);
         }
 
+        m_begin = m_xv.begin()-1; // !!!
 
-        m_begin = m_xv.begin(); // !!!
 
-        std::cout << m_ss.str();
 
     }
 
