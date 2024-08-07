0cb8d335858e00c13c29497caf38fa7df432cc10
xfgt <waffle_man562@yahoo.com>
2024-08-07 12:29:47 +0300

    insertion values to ostream

--- Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
+++ Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
@@ -13,27 +13,19 @@ template <typename T, typename Generator>
 struct Sequence{
     Generator m_gen;
     std::vector<int> m_xv;
-
     std::vector<int>::iterator m_begin;
-
-
-    Sequence(){
-        m_xv.reserve(3); // !
-        m_begin = m_xv.begin();
-    }
-
 
     auto begin() { return m_begin; }
     auto end() { return m_xv.end(); }
 
-
-
     void generateNext(int& n){
 
         for(int i = 0; i < n; i++){
-            m_xv.push_back(m_gen());
+            int x = m_gen();
+            std::cout << x << ' ';
+            m_xv.push_back(x);
         }
-
+        m_begin = m_xv.begin(); // !
 
     }
 
