f5813e5e1b0e92c855b6c9162a3b98a064ef6e05
xfgt <waffle_man562@yahoo.com>
2024-08-07 12:39:41 +0300

    introducing stringstream

--- Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
+++ Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
@@ -7,6 +7,7 @@
 
 
 #include <vector>
+#include <sstream>
 
 
 template <typename T, typename Generator>
@@ -14,19 +15,28 @@ struct Sequence{
     Generator m_gen;
     std::vector<int> m_xv;
     std::vector<int>::iterator m_begin;
+    std::ostringstream m_oss;
 
     auto begin() { return m_begin; }
     auto end() { return m_xv.end(); }
 
     void generateNext(int& n){
+        if(n == 0)
+            return;
+
+        if(!(m_xv.empty()))
+            m_xv.clear();
 
         for(int i = 0; i < n; i++){
             int x = m_gen();
-            std::cout << x << ' ';
             m_xv.push_back(x);
+            m_oss << x << " ";
         }
-        m_begin = m_xv.begin(); // !
 
+
+        m_begin = m_xv.begin(); // !!!
+
+        std::cout << m_oss.str();
     }
 
 };
