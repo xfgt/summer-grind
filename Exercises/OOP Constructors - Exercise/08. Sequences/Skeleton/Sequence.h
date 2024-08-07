1704ab10b1a32be3f0509f55010d4e622430de68
xfgt <waffle_man562@yahoo.com>
2024-08-07 14:28:54 +0300

    restructure if checks

--- Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
+++ Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
@@ -15,30 +15,32 @@ struct Sequence{
     Generator m_gen;
     std::vector<int> m_xv;
     std::vector<int>::iterator m_begin;
-    std::stringstream m_ss;
 
     auto begin() { return m_begin; }
-    auto end() { return --m_xv.end(); } // x-1!
+    auto end()  { return --m_xv.end(); } // x-1!
 
     void generateNext(int& n){
-
 
         if(!(m_xv.empty())){
             for(auto it = m_begin+2; it != m_xv.end(); ++it)
                 std::cout << *it << ' ';
-
             m_xv.clear();
-
         }
 
-
-        for(int i = 0; i <= n; i++){ // = (!!!)
+        //                = !!
+        for(int i = 0; i <= n; i++){
             m_xv.push_back(m_gen());
         }
 
-        m_begin = m_xv.begin()-1; // !!!
 
+        if(m_begin != m_xv.end()){
+            m_begin = m_xv.begin() + 1;
+        }
 
+        if(*m_begin != 0)
+            m_begin = m_xv.begin()-1;
+        else
+            m_begin = m_xv.begin();
 
     }
 
