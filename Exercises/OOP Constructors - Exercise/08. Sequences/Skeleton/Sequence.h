0591676b796fb1ab94a0c422e23d96e4a7f4a7b2
xfgt <waffle_man562@yahoo.com>
2024-08-07 12:07:18 +0300

    fix to single iterator

--- Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
+++ Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
@@ -15,36 +15,26 @@ struct Sequence{
     std::vector<int> m_xv;
 
     std::vector<int>::iterator m_begin;
-    std::vector<int>::iterator m_end;
+
 
     Sequence(){
         m_xv.reserve(3); // !
         m_begin = m_xv.begin();
-        m_end = m_xv.end();
     }
 
 
     auto begin() { return m_begin; }
-    auto end() { return m_end; }
+    auto end() { return m_xv.end(); }
 
-    void reset(){
-        if(m_xv.empty()){
-            m_begin = m_xv.begin();
-            m_end = m_xv.end();
 
-        } else {
-            m_begin = m_xv.begin();
-            --m_end;
-        }
-
-    } // problem with begin
 
     void generateNext(int& n){
 
         for(int i = 0; i < n; i++){
             m_xv.push_back(m_gen());
         }
-        reset();
+
+
     }
 
 };
