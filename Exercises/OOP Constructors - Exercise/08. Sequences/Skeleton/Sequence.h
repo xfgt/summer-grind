8e8d2ad7018e9a3e108e8f8ccb6164311ed969c2
xfgt <waffle_man562@yahoo.com>
2024-08-13 11:06:37 +0300

    zero test two fix

--- Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
+++ Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
@@ -13,37 +13,65 @@
 template <typename T, typename Generator>
 struct Sequence{
     Generator m_gen;
-    std::vector<int> m_xv;
-    std::vector<int>::iterator m_begin;
+    std::vector<T> m_xv;
+    typename std::vector<T>::iterator m_begin;
+    bool cleared = false;
 
-    auto begin() { return m_begin; }
+    auto begin() { return --m_begin; }
     auto end()  { return --m_xv.end(); } // x-1!
 
-    void generateNext(int& n){
+    void generateNext(const int& n){
+
+        // if(m_begin == m_xv.end())
+        //     m_begin = m_xv.begin();
+
 
         if(!(m_xv.empty())){
-            for(auto it = m_begin+2; it != m_xv.end(); ++it)
+            for(auto it = ++m_xv.begin(); it != m_xv.end(); ++it) // +2
                 std::cout << *it << ' ';
             m_xv.clear();
+            m_xv.shrink_to_fit();
+            cleared = true;
         }
 
         //                = !!
-        for(int i = 0; i <= n; i++){
+        for(int i = 0; i <= n; i++)
             m_xv.push_back(m_gen());
+
+        m_xv.shrink_to_fit();
+
+        {
+
+            if(!cleared){
+                if(m_begin != m_xv.end())
+                    m_begin = m_xv.begin();
+            } else {
+                m_begin = m_xv.begin() - 1; // fix autoincrement for-range
+            }
+            cleared = false;
+            return;
+
+            if(m_begin != m_xv.end()){ // first time
+                m_begin = m_xv.begin();
+            } else {
+                m_begin = m_xv.begin()-1;
+            }
+            return;
+
+            if(m_begin == m_xv.end() && *m_begin != 0)
+                m_begin = m_xv.begin()-1;
+
+
         }
 
 
-        if(m_begin != m_xv.end())
-            m_begin = m_xv.begin() + 1;
 
-        if(*m_begin != 0)
-            m_begin = m_xv.begin()-1;
-        else
-            m_begin = m_xv.begin();
 
     }
 
 };
+
+
 
 
 
