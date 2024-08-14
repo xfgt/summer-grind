b25335b232e7ea6a02514b264c1008b57b87ea55
xfgt <waffle_man562@yahoo.com>
2024-08-14 14:00:31 +0300

    prevent reset iterators on pushback

--- Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
+++ Exercises/OOP Constructors - Exercise/08. Sequences/Skeleton/Sequence.h
@@ -7,7 +7,6 @@
 
 
 #include <vector>
-#include <sstream>
 
 
 template <typename T, typename Generator>
@@ -15,54 +14,33 @@ struct Sequence{
     Generator m_gen;
     std::vector<T> m_xv;
     typename std::vector<T>::iterator m_begin;
-    bool cleared = false;
+    typename std::vector<T>::iterator m_end;
 
-    auto begin() { return --m_begin; }
-    auto end()  { return --m_xv.end(); } // x-1!
+
+    auto begin() { return m_begin; }  // for-range auto increment
+    auto end()  { return m_end; }     // actual value
+
 
     void generateNext(const int& n){
 
-        // if(m_begin == m_xv.end())
-        //     m_begin = m_xv.begin();
+        auto h = m_begin;
+        auto r = m_xv.end();
 
-
-        if(!(m_xv.empty())){
-            for(auto it = ++m_xv.begin(); it != m_xv.end(); ++it) // +2
+        if(!m_xv.empty()){
+            for(auto it = ++h; it != r; ++it){
                 std::cout << *it << ' ';
+            }
             m_xv.clear();
             m_xv.shrink_to_fit();
-            cleared = true;
+            m_xv.assign(1, 0);
         }
 
-        //                = !!
         for(int i = 0; i <= n; i++)
             m_xv.push_back(m_gen());
 
-        m_xv.shrink_to_fit();
 
-        {
-
-            if(!cleared){
-                if(m_begin != m_xv.end())
-                    m_begin = m_xv.begin();
-            } else {
-                m_begin = m_xv.begin() - 1; // fix autoincrement for-range
-            }
-            cleared = false;
-            return;
-
-            if(m_begin != m_xv.end()){ // first time
-                m_begin = m_xv.begin();
-            } else {
-                m_begin = m_xv.begin()-1;
-            }
-            return;
-
-            if(m_begin == m_xv.end() && *m_begin != 0)
-                m_begin = m_xv.begin()-1;
-
-
-        }
+        m_begin = m_xv.begin();
+        m_end = m_xv.end()-1;
 
 
 
