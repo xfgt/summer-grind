20f44143eb211ec64f28e764fe6009a6c4adac3c
xfgt <waffle_man562@yahoo.com>
2024-08-26 20:24:23 +0300

    07. MultiplicationTable - solution

---
+++ Exercises/Sofuni_Summer2024/Fundamentals/Basic Syntax - Lab/07_MulTable/main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by twister on 8/26/24.
+//
+
+#include <iostream>
+
+#include "../../../../../../../../../../../../usr/include/math.h"
+
+
+int main(){
+    using namespace std;
+
+    int input{};
+    cin >> input;
+
+    for(int i = 1; i <= 10; i++)
+        cout << input << " X " << i << " = " << input * i  << endl;
+
+
+
+    return 0;
+}
