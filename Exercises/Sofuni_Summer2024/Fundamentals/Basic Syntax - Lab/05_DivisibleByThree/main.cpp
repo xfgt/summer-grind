374523130eb437b6c9f3a9c34af94a5e3aaff767
xfgt <waffle_man562@yahoo.com>
2024-08-26 20:01:41 +0300

    05. divbyThree - sol

---
+++ Exercises/Sofuni_Summer2024/Fundamentals/Basic Syntax - Lab/05_DivisibleByThree/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by twister on 8/26/24.
+//
+
+#include <iostream>
+
+
+int main(){
+    using namespace std;
+
+    for(int i = 1; i <= 100; i++){
+        if( i % 3 == 0)
+            cout << i << endl;
+    }
+
+    return 0;
+}
