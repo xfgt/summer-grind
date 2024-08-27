915f3dacad2b233f4b48c63b6cf8ca032e862ff9
xfgt <waffle_man562@yahoo.com>
2024-08-27 10:41:45 +0300

    04. OneToN - solution

---
+++ Exercises/Sofuni_Summer2024/Fundamentals/Basic Syntax - Exercises/04_OneToN/main.cpp
@@ -0,0 +1,24 @@
+//
+// Created by twister on 8/27/24.
+//
+
+
+
+#include <iostream>
+
+
+int main(){
+
+
+    int n{};
+    std::cin >> n;
+
+    for(int i = 1; i <= n; i++)
+        std::cout << i << ' ';
+
+
+
+
+
+    return 0;
+}
