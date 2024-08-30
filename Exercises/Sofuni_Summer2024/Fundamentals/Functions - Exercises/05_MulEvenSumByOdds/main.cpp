7c78e9faa7b97db13eb70298b69cbd128ac8da73
xfgt <waffle_man562@yahoo.com>
2024-08-30 10:26:24 +0300

    05 - solution

--- Exercises/Sofuni_Summer2024/Fundamentals/Functions - Exercises/05_MulEvenSumByOdds/main.cpp
+++ Exercises/Sofuni_Summer2024/Fundamentals/Functions - Exercises/05_MulEvenSumByOdds/main.cpp
@@ -5,8 +5,35 @@
 
 #include <iostream>
 
+void solution(const std::string& x, size_t sz){
+
+
+
+
+    int evenSum{}, oddSum{};
+
+    for(int i = 0; i < sz; i++){
+        if(!(x[i] >= '0' && x[i] <= '9'))
+            continue;
+
+        if((x[i]-'0') % 2 == 0){
+            evenSum += x[i] - '0';
+        } else{
+            oddSum += x[i] - '0';
+        }
+    }
+
+    std::cout << evenSum * oddSum;
+}
+
 
 int main(){
+
+    std::string number{};
+    std::getline(std::cin, number);
+
+    solution(number, number.size());
+
 
 
     return 0;
