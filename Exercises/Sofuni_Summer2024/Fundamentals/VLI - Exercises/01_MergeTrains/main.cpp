c595fff3373cb7ac0287e7fab0c2d3ab1289315b
xfgt <waffle_man562@yahoo.com>
2024-09-07 15:06:42 +0300

    Fundamentals::VectorsListsIterators - half way checkpoint

--- Exercises/Sofuni_Summer2024/Fundamentals/VLI - Exercises/01_MergeTrains/main.cpp
+++ Exercises/Sofuni_Summer2024/Fundamentals/VLI - Exercises/01_MergeTrains/main.cpp
@@ -2,10 +2,54 @@
 // Created by twister on 9/7/24.
 //
 
+#include <algorithm>
 #include <iostream>
+#include <list>
+
+#include <sstream>
 
 
 int main(){
+
+    std::list<std::pair<int, char>> A, B, Merg;
+
+
+    std::string input{}, x{};
+    std::getline(std::cin, input);
+    std::stringstream trim(input);
+
+    while(trim >> x)
+        A.emplace_back(std::make_pair(std::stoi(x), 'A'));
+    std::cin.clear();
+
+    std::getline(std::cin, input);
+    std::stringstream trimT(input);
+
+    while(trimT >> x)
+        B.emplace_back(std::make_pair(std::stoi(x), 'B'));
+    std::cin.clear();
+
+
+    Merg = A;
+    Merg.merge(B);
+    Merg.sort();
+    std::reverse(Merg.begin(), Merg.end());
+
+
+
+
+
+    for(auto it = --Merg.end(); it != --Merg.begin(); --it)
+        std::cout << it->second;
+
+
+    std::cout << std::endl;
+    for(const auto& it : Merg)
+        std::cout << it.first << ' ';
+
+
+
+
 
 
     return 0;
--- Exercises/Sofuni_Summer2024/Fundamentals/VLI - Exercises/02_Brackets/main.cpp
+++ Exercises/Sofuni_Summer2024/Fundamentals/VLI - Exercises/02_Brackets/main.cpp
@@ -3,10 +3,84 @@
 //
 
 #include <iostream>
+#include <sstream>
 
+
+
+
+bool sol(const std::string& input){
+
+    int squares{}, curly{}, normals{}, type{}, lastType{};
+
+    for(int i = 0; i < input.size(); i++){
+
+        switch (input[i]){
+        case '{':
+        case '}':
+            type = 1;
+            break;
+
+        case '[':
+        case ']':
+            type = 2;
+            break;
+
+        case '(':
+        case ')':
+            type = 3;
+            break;
+        default:
+            break;
+        } // get type
+
+        switch (type){
+        case 1: // {} expects 1, 2, 3
+            curly++;
+            break;
+
+        case 2: //[] 2, 3
+            squares++;
+            break;
+
+        case 3: //() only 3
+            normals++;
+            break;
+
+        default:
+            break;
+        }
+
+
+        if(lastType <= type){
+
+        }
+
+
+
+
+
+        lastType = type;
+    }
+
+}
 
 int main(){
 
+
+
+    // Hierarchy
+    // {} 1
+    // [] 2
+    // () 3
+
+    std::string input{};
+    std::getline(std::cin, input);
+
+    if(sol(input)){
+        std::cout << "valid";
+    } else{
+        std::cout << "invalid";
+    }
 
     return 0;
 }
